65f7d7635f272504d0b54c1a0d035c22014bd36e
Prithivi59 <Prithivi59@users.noreply.github.com>
2023-04-21 15:00:01 +0530

    commiting Student.js

---
+++ Student.js
@@ -0,0 +1,7 @@
+const student = ['Ram','Bheem','Legend'];
+const marks = [56,67,44];
+//console.log(marks);
+
+module.exports = "hello vannkam";
+module.exports = student;
+module.exports = {student,marks} ;
